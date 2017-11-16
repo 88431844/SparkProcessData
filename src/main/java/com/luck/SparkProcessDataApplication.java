8bf79a843a510d806515e9d03fd51c6d4ba33a5b
gaozhihua.aerozhonghuan.com <vv11uhtfsMXT>
2017-11-16 10:00:25 +0800

    Initial commit

---
+++ src/main/java/com/luck/SparkProcessDataApplication.java
@@ -0,0 +1,12 @@
+package com.luck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkProcessDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkProcessDataApplication.class, args);
+	}
+}
